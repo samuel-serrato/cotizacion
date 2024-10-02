7518b89ab6c7e52b760b42ff893facac8e36ec0f
Samuel Serrato <pablosamuel27@hotmail.com>
2024-10-02 00:55:42 -0600

    estadisticas v1, grafica - totalclientesxmes

--- lib/screens/estadisticas.dart
+++ lib/screens/estadisticas.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:cotizacion/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:fl_chart/fl_chart.dart';
@@ -20,143 +21,120 @@ class ClienteData {
   }
 }
 
-// Widget para mostrar el gráfico
-class ClienteChart extends StatefulWidget {
-  final List<ClienteData> data;
-
-  const ClienteChart({Key? key, required this.data}) : super(key: key);
-
-  @override
-  _ClienteChartState createState() => _ClienteChartState();
+double roundUpToNearestMultiple(double value, double multiple) {
+  return (value / multiple).ceil() * multiple;
 }
 
-class _ClienteChartState extends State<ClienteChart> {
-  double _minX = 0;
-  double _maxX = 11; // Suponiendo 12 meses
-  double _zoomFactor = 1.0;
+class ClienteChart extends StatelessWidget {
+  final List<ClienteData> data;
+  final int month;
+  final int year;
 
-  Map<int, Map<String, double>> _totalsByMonthAndClient = {};
-  List<BarChartGroupData> _barGroups = [];
-
-  void _processData() {
-    // Procesar datos
-    for (var clienteData in widget.data) {
-      final monthYear = clienteData.mesAno.split('-');
-      final month = int.parse(monthYear[0]) - 1;
-
-      _totalsByMonthAndClient.putIfAbsent(month, () => {});
-      _totalsByMonthAndClient[month]![clienteData.cliente] =
-          (_totalsByMonthAndClient[month]![clienteData.cliente] ?? 0) + clienteData.totalMes;
-    }
-
-    List<String> clientes = _totalsByMonthAndClient.values.expand((clientesData) => clientesData.keys).toSet().toList();
-
-    for (int month = 0; month < 12; month++) {
-      List<BarChartRodData> barRods = [];
-      for (var cliente in clientes) {
-        double total = _totalsByMonthAndClient[month]?[cliente] ?? 0.0;
-        barRods.add(BarChartRodData(
-          toY: total,
-          color: Colors.primaries[clientes.indexOf(cliente) % Colors.primaries.length].withOpacity(0.7),
-          width: 16,
-          borderRadius: BorderRadius.circular(8),
-        ));
-      }
-      _barGroups.add(BarChartGroupData(x: month, barRods: barRods));
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _processData(); // Procesar datos al iniciar
-  }
+  const ClienteChart({Key? key, required this.data, required this.month, required this.year})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // Filtrar grupos de barras por el rango
-    final filteredBarGroups = _barGroups.where((group) => group.x >= _minX && group.x <= _maxX).toList();
+    List<ClienteData> monthlyData = data.where((d) {
+      final monthYear = d.mesAno.split('-');
+      return int.parse(monthYear[0]) - 1 == month && int.parse(monthYear[1]) == year; // Filtra por mes y año
+    }).toList();
 
-    return GestureDetector(
-      onScaleUpdate: (details) {
-        setState(() {
-          // Zoom in/out
-          if (details.scale != 1.0) {
-            _zoomFactor *= details.scale;
-            _zoomFactor = _zoomFactor.clamp(1.0, 5.0); // Limita el zoom
-          }
-          // Paneo
-          _minX += details.focalPoint.dx > 0 ? 1 : -1;
-          _maxX += details.focalPoint.dx > 0 ? 1 : -1;
-        });
-      },
-      child: SizedBox(
-        height: 400,
-        child: BarChart(
-          BarChartData(
-            barGroups: filteredBarGroups,
-            titlesData: FlTitlesData(
-              bottomTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  getTitlesWidget: (value, meta) {
-                    const months = ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'];
-                    return SideTitleWidget(
-                      axisSide: meta.axisSide,
-                      child: Text(
-                        months[value.toInt()],
-                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.blueGrey),
-                      ),
-                    );
-                  },
+    Map<String, double> totalsByClient = {};
+    for (var clienteData in monthlyData) {
+      totalsByClient[clienteData.cliente] =
+          (totalsByClient[clienteData.cliente] ?? 0) + clienteData.totalMes;
+    }
+
+    if (totalsByClient.isEmpty) {
+      return Center(child: Text('No hay datos para este mes y año.'));
+    }
+
+    List<BarChartGroupData> barGroups = [];
+    int index = 0;
+
+    totalsByClient.forEach((cliente, total) {
+      barGroups.add(BarChartGroupData(
+        x: index++,
+        barRods: [
+          BarChartRodData(
+            toY: total,
+            color: Colors.primaries[index % Colors.primaries.length].withOpacity(0.7),
+            width: 16,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ],
+        showingTooltipIndicators: [0],
+      ));
+    });
+
+    double maxY = roundUpToNearestMultiple(totalsByClient.values.reduce((a, b) => a > b ? a : b), 5000);
+    double minY = 0;
+
+    return SizedBox(
+      height: 300, // Cambia la altura aquí
+      child: Card(
+        elevation: 4,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Container(
+          color: Colors.white, // Color de fondo de la gráfica
+          padding: const EdgeInsets.all(16.0),
+          child: BarChart(
+            BarChartData(
+              minY: minY,
+              maxY: maxY,
+              titlesData: FlTitlesData(
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    getTitlesWidget: (value, meta) {
+                      if (value.toInt() >= totalsByClient.length) return Container();
+                      return SideTitleWidget(
+                        axisSide: meta.axisSide,
+                        child: Text(
+                          totalsByClient.keys.elementAt(value.toInt()),
+                          style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold, color: Colors.blueGrey),
+                        ),
+                      );
+                    },
+                  ),
                 ),
-              ),
-              leftTitles: AxisTitles(
-                sideTitles: SideTitles(
-                  showTitles: true,
-                  interval: 10000,
-                  reservedSize: 60,
-                  getTitlesWidget: (value, meta) {
-                    return SideTitleWidget(
-                      axisSide: meta.axisSide,
-                      child: Text(
-                        '${value.toInt()}',
-                        style: TextStyle(fontSize: 12, color: Colors.blueGrey, fontWeight: FontWeight.bold),
-                      ),
-                    );
-                  },
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: true,
+                    interval: 10000,
+                    reservedSize: 60,
+                    getTitlesWidget: (value, meta) {
+                      return SideTitleWidget(
+                        axisSide: meta.axisSide,
+                        child: Text(
+                          '${value.toInt()}',
+                          style: TextStyle(fontSize: 12, color: Colors.blueGrey, fontWeight: FontWeight.bold),
+                        ),
+                      );
+                    },
+                  ),
                 ),
+                rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+                topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
               ),
-              rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-              topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
-            ),
-            gridData: FlGridData(
-              show: true,
-              horizontalInterval: 10000,
-              getDrawingHorizontalLine: (value) {
-                return FlLine(color: Colors.grey.withOpacity(0.2), strokeWidth: 1, dashArray: [5, 5]);
-              },
-              drawVerticalLine: false,
-            ),
-            borderData: FlBorderData(
-              show: true,
-              border: Border(
-                left: BorderSide(color: Colors.grey.shade300, width: 1),
-                bottom: BorderSide(color: Colors.grey.shade300, width: 1),
-              ),
-            ),
-            barTouchData: BarTouchData(
-              touchTooltipData: BarTouchTooltipData(
-                tooltipMargin: 8,
-                getTooltipItem: (group, groupIndex, rod, rodIndex) {
-                  String clienteName = _totalsByMonthAndClient[group.x]!.keys.elementAt(rodIndex);
-                  double totalValue = rod.toY;
-                  return BarTooltipItem(
-                    '$clienteName\n\$${totalValue.toStringAsFixed(2)}',
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 14),
-                  );
+              gridData: FlGridData(
+                show: true,
+                horizontalInterval: 10000,
+                getDrawingHorizontalLine: (value) {
+                  return FlLine(color: Colors.grey.withOpacity(0.2), strokeWidth: 1, dashArray: [5, 5]);
                 },
+                drawVerticalLine: false,
               ),
+              borderData: FlBorderData(
+                show: true,
+                border: Border(
+                  left: BorderSide(color: Colors.grey.shade300, width: 1),
+                  bottom: BorderSide(color: Colors.grey.shade300, width: 1),
+                ),
+              ),
+              barGroups: barGroups,
+              barTouchData: barTouchData,
             ),
           ),
         ),
@@ -164,6 +142,27 @@ class _ClienteChartState extends State<ClienteChart> {
     );
   }
 }
+
+ 
+BarTouchData get barTouchData => BarTouchData(
+      enabled: true,
+      touchTooltipData: BarTouchTooltipData(
+        getTooltipItem: (
+          BarChartGroupData group,
+          int groupIndex,
+          BarChartRodData rod,
+          int rodIndex,
+        ) {
+          return BarTooltipItem(
+            '${rod.toY.round()}',
+            TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          );
+        },
+      ),
+    );
 
 // Pantalla principal
 class EstadisticasScreen extends StatefulWidget {
@@ -173,13 +172,18 @@ class EstadisticasScreen extends StatefulWidget {
   _EstadisticasScreenState createState() => _EstadisticasScreenState();
 }
 
-class _EstadisticasScreenState extends State<EstadisticasScreen> {
+class _EstadisticasScreenState extends State<EstadisticasScreen>
+    with SingleTickerProviderStateMixin {
   late Future<List<ClienteData>> futureData;
+  late int selectedMonth;
+  late int selectedYear;
 
   @override
   void initState() {
     super.initState();
     futureData = fetchClienteData();
+    selectedMonth = 9; // Iniciar en el primer mes
+    selectedYear = DateTime.now().year; // Año actual
   }
 
   Future<List<ClienteData>> fetchClienteData() async {
@@ -192,10 +196,58 @@ class _EstadisticasScreenState extends State<EstadisticasScreen> {
     }
   }
 
+  void updateMonth(int month) {
+    setState(() {
+      selectedMonth = month;
+    });
+  }
+
+  // Método para seleccionar solo el año
+  Future<void> selectYear(BuildContext context) async {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Selecciona un año'),
+          content: SizedBox(
+            height: 200,
+            width: 200,
+            child: ListView.builder(
+              itemCount: 20, // Puedes ajustar el número de años que desees mostrar
+              itemBuilder: (context, index) {
+                int year = DateTime.now().year - index; // Años desde el actual hacia atrás
+                return ListTile(
+                  title: Text(year.toString()),
+                  onTap: () {
+                    setState(() {
+                      selectedYear = year; // Actualiza el año seleccionado
+                    });
+                    Navigator.of(context).pop(); // Cierra el diálogo
+                  },
+                );
+              },
+            ),
+          ),
+        );
+      },
+    );
+  }
+ bool _isDarkMode = false; // Estado del modo oscuro
+  
+    void _toggleDarkMode(bool value) {
+    setState(() {
+      _isDarkMode = value;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Estadísticas de Clientes')),
+      backgroundColor: Color(0xFFf7f8fa),
+      appBar: CustomAppBar(
+          isDarkMode: _isDarkMode,
+          toggleDarkMode: _toggleDarkMode,
+          title: 'Estadísticas', // Título específico para esta pantalla
+        ),
       body: FutureBuilder<List<ClienteData>>(
         future: futureData,
         builder: (context, snapshot) {
@@ -204,18 +256,68 @@ class _EstadisticasScreenState extends State<EstadisticasScreen> {
           } else if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           } else {
-            return Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  Text(
-                    'Consumo Total por Cliente por Mes',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            return Column(
+              children: [
+                // Fila de selección de meses y año
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Selector de Meses
+                      Row(
+                        children: List.generate(12, (index) {
+                          const months = [
+                            'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 
+                            'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+                          ];
+                          return GestureDetector(
+                            onTap: () => updateMonth(index),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: selectedMonth == index ? Color(0xFF001F3F) : Colors.grey[200],
+                                borderRadius: BorderRadius.circular(12),
+                                boxShadow: selectedMonth == index
+                                    ? [BoxShadow(color: Colors.blueAccent.withOpacity(0.3), blurRadius: 6, offset: Offset(0, 3))]
+                                    : null,
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 10.0),
+                              margin: const EdgeInsets.only(right: 8.0), // Espacio entre meses
+                              child: Text(
+                                months[index],
+                                style: TextStyle(
+                                  color: selectedMonth == index ? Colors.white : Colors.black87,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 16,
+                                ),
+                              ),
+                            ),
+                          );
+                        }),
+                      ),
+
+                      // Selector de Año
+                      Row(
+                        children: [
+                          Text(
+                            'Año: $selectedYear',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.calendar_today),
+                            onPressed: () => selectYear(context), // Muestra el selector de año
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 10),
-                  ClienteChart(data: snapshot.data!),
-                ],
-              ),
+                ),
+
+                // Gráfico de Clientes
+                Expanded(
+                  child: ClienteChart(data: snapshot.data!, month: selectedMonth, year: selectedYear),
+                ),
+              ],
             );
           }
         },
